c9964a52b31ef8c8b52f0671258569b9c96ce319
amstokely <andystokely@gmail.com>
2025-08-04 17:13:05 -0600

    Finished test suite for get_interval_type.

--- test/unity/test_variable_stream_interval.c
+++ test/unity/test_variable_stream_interval.c
@@ -1,34 +1,101 @@
 #include "unity.h"
-#include "dynamic_output_interval_stream.h"
+#include "xml_stream_parser.h"
+#include "ezxml.h"
 #include <stdlib.h>
 #include <string.h>
 
+static ezxml_t root = NULL;
 static char *test_xml = NULL;
-static Stream streams[MAX_STREAMS];
-static int num_streams = 0;
 
 void setUp(void) {
-    const char *xml_src =
-        "  <stream name=\"variable_output\" type=\"output\" filename_template=\"output.nc\">"
-        "    <output_interval>"
-        "      <range start=\"0000_00:00:00\" end=\"0005_00:00:00\" interval=\"0_01:00:00\"/>"
-        "    </output_interval>"
-        "  </stream>";
-
-    test_xml = strdup(xml_src);
-    num_streams = parse_streams(streams, test_xml);
 }
 
 void tearDown(void) {
+    ezxml_free(root);
     free(test_xml);
     test_xml = NULL;
+    root = NULL;
 }
 
-void test_check_output_interval_type(void) {
+// -- Test: variable interval --
+void test_variable_interval(void) {
+    const char *xml = "<streams>"
+            "  <stream name=\"s1\" type=\"output\" filename_template=\"f.nc\">"
+            "    <output_interval>"
+            "      <range start=\"0000_00:00:00\" end=\"0001_00:00:00\" interval=\"0_01:00:00\"/>"
+            "    </output_interval>" "  </stream>" "</streams>";
+
+    test_xml = strdup(xml);
+    root = ezxml_parse_str(test_xml, strlen(test_xml));
+    ezxml_t stream = ezxml_child(root, "stream");
+
+    TEST_ASSERT_EQUAL_INT(
+        INTERVAL_VARIABLE, get_interval_type(stream, "output_interval")
+    );
+}
+
+// -- Test: fixed interval --
+void test_fixed_interval(void) {
+    const char *xml = "<streams>"
+            "  <stream name=\"s2\" type=\"output\" output_interval=\"00:15:00\" />"
+            "</streams>";
+
+    test_xml = strdup(xml);
+    root = ezxml_parse_str(test_xml, strlen(test_xml));
+    ezxml_t stream = ezxml_child(root, "stream");
+
+    TEST_ASSERT_EQUAL_INT(
+        INTERVAL_FIXED, get_interval_type(stream, "output_interval")
+    );
+}
+
+// -- Test: both fixed and variable (conflict) --
+void test_conflicting_interval(void) {
+    const char *xml = "<streams>"
+            "  <stream name=\"s3\" type=\"output\" output_interval=\"00:30:00\">"
+            "    <output_interval>"
+            "      <range start=\"0000_00:00:00\" end=\"0002_00:00:00\" interval=\"0_01:00:00\"/>"
+            "    </output_interval>" "  </stream>" "</streams>";
+
+    test_xml = strdup(xml);
+    root = ezxml_parse_str(test_xml, strlen(test_xml));
+    ezxml_t stream = ezxml_child(root, "stream");
+
+    TEST_ASSERT_EQUAL_INT(
+        INTERVAL_CONFLICT, get_interval_type(stream, "output_interval")
+    );
+}
+
+// -- Test: missing interval --
+void test_missing_interval(void) {
+    const char *xml = "<streams>"
+            "  <stream name=\"s4\" type=\"output\" />" "</streams>";
+
+    test_xml = strdup(xml);
+    root = ezxml_parse_str(test_xml, strlen(test_xml));
+    ezxml_t stream = ezxml_child(root, "stream");
+
+    TEST_ASSERT_EQUAL_INT(
+        INTERVAL_NOT_FOUND, get_interval_type(stream, "output_interval")
+    );
+}
+
+// -- Test: NULL arguments --
+void test_null_args(void) {
+    TEST_ASSERT_EQUAL_INT(
+        INTERVAL_NOT_FOUND, get_interval_type(NULL, "output_interval")
+    );
+    TEST_ASSERT_EQUAL_INT(
+        INTERVAL_NOT_FOUND, get_interval_type(NULL, NULL)
+    );
 }
 
 int main(void) {
     UNITY_BEGIN();
-    RUN_TEST(test_check_output_interval_type);
+    RUN_TEST(test_variable_interval);
+    RUN_TEST(test_fixed_interval);
+    RUN_TEST(test_conflicting_interval);
+    RUN_TEST(test_missing_interval);
+    RUN_TEST(test_null_args);
     return UNITY_END();
 }
